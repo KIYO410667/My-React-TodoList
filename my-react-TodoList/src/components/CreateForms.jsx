475731bd2e7eeb3d521abd6a746c6409353aee60
D0987379 <hyou0218@gmail.com>
2024-07-27 02:07:37 +0800

    Create input form and its css

---
+++ my-react-TodoList/src/components/CreateForms.jsx
@@ -0,0 +1,10 @@
+function CreateForms() {
+  return (
+    <form className="form-container">
+      <input type="text" placeholder="輸入要買的東西"></input>
+      <button type="submit">加入</button>
+    </form>
+  );
+}
+
+export default CreateForms
