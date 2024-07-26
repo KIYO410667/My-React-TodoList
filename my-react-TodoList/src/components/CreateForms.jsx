feb2aca510c128410eb64b0b22d4be66bcf0bdf5
D0987379 <hyou0218@gmail.com>
2024-07-27 03:21:46 +0800

    - UseState to make two-way binding in input form

--- my-react-TodoList/src/components/CreateForms.jsx
+++ my-react-TodoList/src/components/CreateForms.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 function CreateForms() {
+
+  const [inputText, setInputText] = useState('');
+
+  const handleTodo = (e) => {
+    setInputText(e.target.value);
+  }
+
   return (
     <form className="form-container">
-      <input type="text" placeholder="輸入要買的東西"></input>
+      <input type="text" placeholder="輸入要買的東西" 
+      value={inputText} onChange={handleTodo}></input>
       <button type="submit">加入</button>
     </form>
   );
