f25792885ebb39b641488fc8c3e09af9cebd8064
D0987379 <hyou0218@gmail.com>
2024-07-27 23:31:47 +0800

    Add delete Icon and functionality

--- my-react-TodoList/src/components/BucketList.jsx
+++ my-react-TodoList/src/components/BucketList.jsx
@@ -1,6 +1,13 @@
-function BucketList({bucket}) {
+import { MdOutlineDeleteOutline } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+
+function BucketList({ bucket, deleteBucket }) {
   return <div className="bucket">
     <p>{bucket.inputText}</p>
+    <div><MdEdit style={{marginLeft: '5px', cursor: 'pointer'}}/>
+    <MdOutlineDeleteOutline onClick={() => {deleteBucket(bucket.id)}}
+    style={{marginLeft: '5px', cursor: 'pointer'}}/>
+    </div>
   </div>
 }
 
--- my-react-TodoList/src/components/TodoWrapper.jsx
+++ my-react-TodoList/src/components/TodoWrapper.jsx
@@ -3,20 +3,33 @@ import BucketList from "./BucketList";
 import CreateForms from "./CreateForms";
 
 function TodoWrapper() {
+  const [buckets, setBuckets] = useState([]);
 
-  const [buckets, setBuckets]= useState([
-  ]);
+  const handleBucket = (inputText) => {
+    setBuckets([...buckets, 
+      { inputText: inputText, id: Math.random(), isCompleted: false}]);
+  };
 
-  const handleBucket = (inputText) =>{
-    setBuckets([...buckets, {inputText: inputText, id: Math.random()}]);
-  }
+  const deleteBucket = (id) => {
+    setBuckets(
+      buckets.filter((bucket) => {
+        return bucket.id !== id;
+      })
+    );
+  };
 
   return (
     <div className="wrapper">
       <h1>購物清單</h1>
-      <CreateForms handleBucket={handleBucket}/>
+      <CreateForms handleBucket={handleBucket} />
       {buckets.map((bucket) => {
-        return <BucketList bucket={bucket} />
+        return (
+          <BucketList
+            bucket={bucket}
+            key={bucket.id}
+            deleteBucket={deleteBucket}
+          />
+        );
       })}
     </div>
   );
